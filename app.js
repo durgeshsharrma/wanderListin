5a5ab94cb6324027e965b2cba58ce2846559e563
durgeshsharrma <durgeshshrma14@gmail.com>
2025-05-15 19:46:21 +0530

    app.js changes

--- app.js
+++ app.js
@@ -193,7 +193,7 @@ main()
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect(process.env.MONGO_URI);
 }
 
 
@@ -238,6 +238,6 @@ app.use((err , req , res , next) => {
 
 })
 
-app.listen( 8000 , () => {
+app.listen( process.env.PORT || 8000 , () => {
     console.log(`server is listening on port 8080 `);
 });
